b739c1f84a3a64691c1a279275c572958b0de7ed
suenghuny <suenghuny@naver.com>
2025-03-22 15:21:45 +0900

    kl_divergence penalty 추가

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ if vessl_on == True:
 else:
     import wandb
     wandb.login()
-    wandb.init(project="second-eigen", name="experiment-1_noise_annealing")
+    wandb.init(project="second-eigen", name="experiment-1_noise_annealing_with_kl_divergence_penalty")
 
 
     output_dir = "output/"
