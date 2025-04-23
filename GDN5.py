027e664908fc8954ef45b38513a5cc4b21681f03
suenghuny <suenghuny@naver.com>
2025-04-23 18:36:14 +0900

    checkpoint(SAC 구조?6-SEED 변경)

--- GDN5.py
+++ GDN5.py
@@ -212,9 +212,9 @@ class Agent(nn.Module):
                  min_graph_variance,
                  env
                  ):
-        torch.manual_seed(54)
-        random.seed(54)
-        np.random.seed(54)
+        torch.manual_seed(24)
+        random.seed(24)
+        np.random.seed(24)
         super(Agent, self).__init__()
         self.num_agent = num_agent
         self.num_enemy = num_enemy
